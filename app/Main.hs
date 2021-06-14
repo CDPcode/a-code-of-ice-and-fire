0ffc16980c59189624dba05ba6cfba2a0c4bfc28
cccsar <15-11295@usb.ve>
2021-06-14 19:31:27 -0400

    added basic main

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,19 @@ module Main where
 
 import Lib
 import Westeros.SouthOfTheWall.Lexer (scanTokens)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do 
+    args <- getArgs
+
+    case head args of 
+        "--lex"   -> lex
+        "--parse" -> undefined
+        _         -> putStrLn "Invalid option"
+
+lex :: IO ()
+lex = do
     str <- getContents  
     case scanTokens str of 
         Left errs -> mapM_ print errs
