13c7c724531af559c80cafa0fa4feb595c46629d
MarcosJLR <socram.693@gmail.com>
2021-06-20 14:32:03 -0400

    feat(symtable): lookup function for symtable

--- src/Westeros/SouthOfTheWall/Symtable.hs
+++ src/Westeros/SouthOfTheWall/Symtable.hs
@@ -3,7 +3,7 @@ module Westeros.SouthOfTheWall.Symtable where
 import qualified Westeros.SouthOfTheWall.Tokens as Tk
 
 import qualified Data.Map.Strict as M
-import Control.Monad.RWS ( MonadState(put, get), RWST )
+import Control.Monad.RWS ( MonadState(put, get), RWST, when )
 
 import Data.List (intercalate, find)
 import Data.Maybe (fromJust)
@@ -31,7 +31,7 @@ data Category -- anything with a name
 
 data AdditionalInfo
     = AliasMD AliasType Type               -- For aliases we save: name and necessary info of the sinonymed type
-    | FunctionMD FunctionInfo -- For functions we save: name , number of arguments, and return type(s). 
+    | FunctionMD FunctionInfo -- For functions we save: name , number of arguments, and return type(s).
     | PassType String                      -- For parameters we save: name, type and either it is value or reference passed
    deriving (Show,Eq)
 
@@ -40,14 +40,14 @@ getFunctionMD SymbolInfo { additional = Just (FunctionMD e) } =  e
 getFunctionMD _                             = error "getFunctionMD: Unpropper use. Report use"
 
 data FunctionInfo = FunctionInfo
-    { nArgs :: Int 
+    { nArgs :: Int
     , fParameters :: [Parameter]
     , fReturn :: [Type]
     , discriminant :: Bool
     } deriving (Show,Eq)
 
-completeFunctionEntry :: FunctionInfo -> Bool 
-completeFunctionEntry fInfo = null (fParameters fInfo) && null (fReturn fInfo) 
+completeFunctionEntry :: FunctionInfo -> Bool
+completeFunctionEntry fInfo = null (fParameters fInfo) && null (fReturn fInfo)
 
 --
 
@@ -81,7 +81,7 @@ type Parameter = (PassType, Id, Type)
 data SymbolInfo = SymbolInfo
     { category :: Category
     , scope :: Int
-    , tp :: Maybe String                 -- pointer to a table entry (the type)
+    , tp :: Maybe Type               -- pointer to a table entry (the type)
     , additional :: Maybe AdditionalInfo
 } deriving Eq
 
@@ -104,7 +104,7 @@ data SymbolTable = SymbolTable
     { dict :: Dict
     , scopeStack :: [Int]
     , nextScope :: Int    -- indicates the next scope to assign
-    -- , additional :: someType 
+    -- , additional :: someType
     }
 
 instance Show SymbolTable where
@@ -192,16 +192,16 @@ insertST st entry = st { dict = insertDict (dict st) entry }
 
 searchAndReplaceSymbol :: SymbolTable -> Entry -> SymbolInfo -> SymbolTable
 searchAndReplaceSymbol st entry@(name,info) newInfo = st { dict = newDict }
-    where            
+    where
         newDict = M.insert name (fromJust newList) (dict st)
-        newList = do 
-           oldList <- findSymbol st name 
+        newList = do
+           oldList <- findSymbol st name
            return (searchAndReplace newInfo info oldList)
-        
+
 
 searchAndReplace :: Eq a => a -> a -> [a] -> [a]
-searchAndReplace _   _   [] = [] 
-searchAndReplace new old (x:xs) 
+searchAndReplace _   _   [] = []
+searchAndReplace new old (x:xs)
  | x == old  = new : xs
  | otherwise = x : searchAndReplace new old xs
 
@@ -269,14 +269,42 @@ statefullSTupdate entry@(name,info) = do
                         Nothing      -> put $ insertST ( st { nextScope = succ (nextScope st) } ) entry
                         Just entries -> do
 
-                            let actualFunctions  = filter (\e -> category e == Function) entries 
+                            let actualFunctions  = filter (\e -> category e == Function) entries
                                 functionsEntries = map getFunctionMD actualFunctions
                                 functionsArgs    = map nArgs functionsEntries
                                 currentArgs      = nArgs ( getFunctionMD info )
 
                             if currentArgs `notElem` functionsArgs then
                                 put $ insertST ( st { nextScope = succ (nextScope st) } ) entry
-
                                 else let errMsg = "A function with the same name \""++name++"\" and # of arguments was already declared"
                                      in fail errMsg
         _         -> fail "Unexpected behaviour: Only Alias and Function categories are relevant in pre-parser"
+
+openScope :: MonadParser ()
+openScope = do
+    symT <- get
+    let newScope = nextScope symT
+    let newStack = newScope : scopeStack symT
+    put $ symT { scopeStack = newStack, nextScope = succ newScope }
+
+closeScope :: MonadParser ()
+closeScope = do
+    symT <- get
+    let (_:newStack) = scopeStack symT
+    put $ symT { scopeStack = newStack }
+
+findBest :: [SymbolInfo] -> [Int] -> Maybe SymbolInfo
+findBest entries [] = Nothing
+findBest entries (s:ss) = case filter (\e -> scope e == s) entries of
+    [] -> findBest entries ss
+    [a] -> Just a
+    _ -> error "Somehow more than one id with same scope"
+
+lookup :: Symbol -> MonadParser (Maybe SymbolInfo)
+lookup sym = do
+    symT <- get
+    let stack = scopeStack symT
+    let mBucket = findSymbol symT sym
+    case mBucket of
+        Nothing -> return Nothing
+        Just bucket -> return $ findBest bucket stack
