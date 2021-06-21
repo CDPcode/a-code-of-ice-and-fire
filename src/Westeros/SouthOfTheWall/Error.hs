a2c005f7728259a42ec85d30bcc9194f7ccf1f60
cccsar <15-11295@usb.ve>
2021-06-21 12:42:08 -0400

    cleaned unnecesary datatypes

--- src/Westeros/SouthOfTheWall/Error.hs
+++ src/Westeros/SouthOfTheWall/Error.hs
@@ -3,14 +3,6 @@ module Westeros.SouthOfTheWall.Error where
 import qualified Westeros.SouthOfTheWall.Tokens as Tk (Token (..))
 
 
-newtype Context a = Context { context :: (a,Tk.Token) }
-
-class CompilerError a where
-    buildContext :: a -> Context a 
-
-instance (Show a) => Show ( Context a ) where
-    show e = undefined
-
 data ParserError  
     = FDefinitionWithoutDeclaration
     | FRepeatedDeclarations
